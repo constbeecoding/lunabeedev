16bdd2ae14c8afeb92a85ef80ba3523d54ba0882
Luna Barber <lunabbarber@gmail.com>
2025-10-22 17:40:00 -0600

    chore(deps): Upgrade 11ty to v.3, update deps, remove old .github code, add package-lock, update eleventy.js to use modules and modern js.

--- .eleventy.js
+++ .eleventy.js
@@ -1,33 +1,31 @@
-const { DateTime } = require("luxon");
-const CleanCSS = require("clean-css");
-const UglifyJS = require("uglify-js");
-const htmlmin = require("html-minifier");
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
+import { DateTime } from "luxon";
+import CleanCSS from "clean-css";
+import UglifyJS from "uglify-js";
+import htmlmin from "html-minifier";
+import markdownIt from "markdown-it";
+import markdownItAnchor from "markdown-it-anchor";
+import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
-module.exports = function(eleventyConfig) {
-  // If you have other `addPlugin` calls, it’s important that UpgradeHelper is added last.
-  eleventyConfig.addPlugin(UpgradeHelper);
-};
 
-module.exports = function(eleventyConfig) {
+export default function(eleventyConfig) {
+
+  // Eleventy Image Plugin https://www.11ty.dev/docs/plugins/image/
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin)
 
   // Eleventy Navigation https://www.11ty.dev/docs/plugins/navigation/
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-
-  // Configuration API: use eleventyConfig.addLayoutAlias(from, to) to add
-  // layout aliases! Say you have a bunch of existing content using
-  // layout: post. If you don’t want to rewrite all of those values, just map
-  // post to a new file like this:
-  // eleventyConfig.addLayoutAlias("post", "layouts/my_new_post_layout.njk");
 
   // Merge data instead of overriding
   // https://www.11ty.dev/docs/data-deep-merge/
   eleventyConfig.setDataDeepMerge(true);
 
+  // Blog posts collection
+eleventyConfig.addCollection("post", collection => {
+  return collection.getFilteredByGlob("posts/*.md").reverse();
+});
+
   // Add support for maintenance-free post authors
-  // Adds an authors collection using the author key in our post frontmatter
-  // Thanks to @pdehaan: https://github.com/pdehaan
   eleventyConfig.addCollection("authors", collection => {
     const blogs = collection.getFilteredByGlob("posts/*.md");
     return blogs.reduce((coll, post) => {
@@ -86,10 +84,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("static/img");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("_includes/assets/css/inline.css");
+  eleventyConfig.addPassthroughCopy("static/js");
 
   /* Markdown Plugins */
-  let markdownIt = require("markdown-it");
-  let markdownItAnchor = require("markdown-it-anchor");
   let options = {
     html: true,
     breaks: true,
@@ -105,13 +102,7 @@ module.exports = function(eleventyConfig) {
 
   return {
     templateFormats: ["md", "njk", "html", "liquid"],
-
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about it.
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for URLs (it does not affect your file structure)
     pathPrefix: "/",
-
     markdownTemplateEngine: "liquid",
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk",
